e409d40b56bab94837bd7394c4c63a0122e68e30
vitoria.cst <vitoria.cst.holanda@gmail.com>
2025-04-26 18:36:13 -0300

    feat: :construction: creating the request hooks for drinks and food

--- src/Hooks/UseApi.ts
+++ src/Hooks/UseApi.ts
@@ -1,5 +1,43 @@
-const getProducts = async() => {
-  fetch('https://fakestoreapi.com/products')
-  .then(response => response.json())
-  .then(data => console.log(data));
-}
+export const getDrinksByCategory = async (category: string) => {
+  try {
+    const response = await fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${encodeURIComponent(
+        category
+      )}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log('Dados recebidos:', data);
+
+    return data;
+  } catch (error) {
+    console.error('Erro ao buscar drinks:', error);
+    return null;
+  }
+};
+
+export const getMealByCategory = async (category: string) => {
+  try {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${encodeURIComponent(
+        category
+      )}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log('Dados recebidos:', data);
+
+    return data;
+  } catch (error) {
+    console.error('Erro ao buscar drinks:', error);
+    return null;
+  }
+};
