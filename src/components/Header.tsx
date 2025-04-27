52c5535ece7a6e698142e14c1c08890652d80d64
vitoria.cst <vitoria.cst.holanda@gmail.com>
2025-04-26 21:39:24 -0300

    feat: :sparkles: Making the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,23 @@
-export const Header = () => {
+import { BookHeart, SquareUser } from 'lucide-react';
+
+interface HeaderProps {
+  isLoginPage: boolean;
+}
+
+export const Header = ({ isLoginPage }: HeaderProps) => {
   return (
-    <header>
-      <div>
-        <h1>Saborize</h1>
-      </div>
+    <header className="p-4 bg-red-900 text-white flex items-center md:justify-between justify-around">
+      <h1 className="text-2xl">Saborize</h1>
+      {!isLoginPage && (
+        <div className="flex gap-2">
+          <button className=" transition-transform duration-300 ease-in-out hover:-translate-y-1 cursor-pointer">
+            <BookHeart />
+          </button>
+          <button className="transition-transform duration-300 ease-in-out hover:-translate-y-1 cursor-pointer">
+            <SquareUser />
+          </button>
+        </div>
+      )}
     </header>
   );
 };
